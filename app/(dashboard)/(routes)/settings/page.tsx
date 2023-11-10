6d2123d3bd53e8919b70280c7047b5f39c1448f1
ombhojane <ombhojane05@gmail.com>
2023-11-11 02:12:26 +0530

    aboutus

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,8 +1,9 @@
-import { Settings } from "lucide-react";
+import { Info, Settings } from "lucide-react";
 
 import { Heading } from "@/components/heading";
 import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
+import { info } from "console";
 
 const SettingsPage = async () => {
   const isPro = await checkSubscription();
@@ -10,18 +11,15 @@ const SettingsPage = async () => {
   return ( 
     <div>
       <Heading
-        title="Settings"
-        description="Manage account settings."
-        icon={Settings}
+        title="About Healthy Plants"
+        description="Plant disease detection, preventions, remedies."
+        icon={Info}
         iconColor="text-gray-700"
         bgColor="bg-gray-700/10"
       />
-      <div className="px-4 lg:px-8 space-y-4">
-        <div className="text-muted-foreground text-sm">
-          {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
-        </div>
-        <SubscriptionButton isPro={isPro} />
-      </div>
+<div className="px-4 md:px-20 lg:px-32 space-y-4">
+  <p>Information comming soon.</p>
+    </div>
     </div>
    );
 }
